2eb5b73ee396bff5c9196b4155920461be423a2d
Dangare Akash <112281257+AkashDangare911@users.noreply.github.com>
2024-01-25 23:09:07 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Queue using stack/queue-using-stack.cpp
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Queue {
+    stack<int> input, output;
+public:
+
+    void enqueue(int x) {
+        input.push(x);
+    }
+
+    int dequeue() 
+    {
+        // put all elements in another stack
+        while(!input.empty())
+        {
+            output.push(input.top());
+            input.pop();
+        }
+        
+        // top element will be front of queue
+        // store it and delete it
+        int ans = output.top();
+        output.pop();
+        
+        
+        // put the elements in original stack
+        while(!output.empty())
+        {
+            input.push(output.top());
+            output.pop();
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        Queue ob;
+
+        int Q;
+        cin>>Q;
+        while(Q--){
+        int QueryType=0;
+        cin>>QueryType;
+        if(QueryType==1)
+        {
+            int a;
+            cin>>a;
+            ob.enqueue(a);
+        }
+        else if(QueryType==2)
+        {
+            cout<<ob.dequeue()<<" ";
+
+        }
+        }
+    cout<<endl;
+    }
+}
+
+// } Driver Code Ends
